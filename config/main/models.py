714b08e3dff8802b050e9d46aec1994202104210
Banzaika <herovichrick@gmail.com>
2023-01-30 22:44:53 +0300

    set media dir and create order model

--- config/main/models.py
+++ config/main/models.py
@@ -7,6 +7,7 @@ class Category(models.Model):
 class ProductShots(models.Model):
     name = models.CharField('Название', max_length=150)
     image = models.ImageField(upload_to='product_shots/')
+    product = models.ForeignKey("Product", verbose_name="Товар", on_delete=models.CASCADE, blank=True, null=True)
 
 class Component(models.Model):
     name = models.CharField(max_length=150)
@@ -17,8 +18,20 @@ class Product(models.Model):
     price = models.DecimalField(verbose_name="Цена", max_digits=7, decimal_places=2)
     category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE)
     poster = models.ImageField(upload_to="posters/")
-    image = models.ForeignKey(ProductShots, verbose_name="Фото товара", on_delete=models.CASCADE)
     components = models.ManyToManyField(Component, verbose_name="Составляющее")
+
+
+class Order(models.Model):
+    STATUSES = (
+        ('a', 'Обрабатывается'),
+        ('b', 'Отменен'),
+        ('c', 'Передано в доставку'),
+        ('d', 'Доставлено'),
+        )
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    products = models.ManyToManyField("Product", verbose_name="Товар")
+    date = models.DateTimeField("Дата и время", auto_now_add=True)
+    status = models.CharField("Статус", max_length=1, choices=STATUSES)
 
 class Cart_relation(models.Model):
     user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
